4f2343bb9e35d23b7ddefef60e30d9910e74bc35
Tom Clement <mr@tomclement.nl>
2015-12-24 13:12:14 +0100

    Added TPP survey controller w/ URL parameter

--- app/survey/survey.js
+++ app/survey/survey.js
@@ -1,24 +1,26 @@
 (function (angular) {
-  "use strict";
+    "use strict";
 
-  var app = angular.module('myApp.survey', ['ngRoute', 'firebase.utils', 'firebase']);
+    var app = angular.module('myApp.survey', ['ngRoute', 'firebase.utils', 'firebase']);
 
-  app.controller('SurveyCtrl', ['$scope', 'messageList', function($scope, messageList) {
-      
+    app.controller('SurveyCtrl', ['$scope', '$routeParams', function ($scope, messageList, $routeParams) {
+        var surveyHeaderID = $routeParams.surveyHeaderID;
 
+        /* TODO 1: Check if user already has this survey in their profile */
+
+        /* TODO 2: If user does not yet have survey in their profile, add it */
+
+        /* TODO 3: Fetch /SurveyHeader/<surveyHeaderID> and check its type (template.nameSurvey) */
+
+        /* TODO 4: Redirect to corresponding Survey controller (TPP, etc) */
     }]);
 
-  app.factory('messageList', ['fbutil', '$firebaseArray', function(fbutil, $firebaseArray) {
-    var ref = fbutil.ref('messages').limitToLast(10);
-    return $firebaseArray(ref);
-  }]);
-
-  app.config(['$routeProvider', function($routeProvider) {
-    $routeProvider.whenAuthenticated('/survey', {
-      templateUrl: 'app/survey/survey.html',
-      controller: 'SurveyCtrl'
-    });
-  }]);
+    app.config(['$routeProvider', function ($routeProvider) {
+        $routeProvider.whenAuthenticated('/survey/:surveyHeaderID', {
+            templateUrl: 'app/survey/survey.html',
+            controller: 'SurveyCtrl'
+        });
+    }]);
 
 
 })(angular);
---
+++ app/survey/tpp/tpp.js
@@ -0,0 +1,26 @@
+(function (angular) {
+    "use strict";
+
+    var app = angular.module('myApp.survey', ['ngRoute', 'firebase.utils', 'firebase']);
+
+    app.controller('TppSurveyCtrl', ['$scope', 'user', '$routeParams', function($scope, user, $routeParams) {
+        var surveyHeaderID = $routeParams.surveyHeaderID;
+
+
+        debugger;
+    }]);
+
+    app.config(['$routeProvider', function($routeProvider) {
+        $routeProvider.whenAuthenticated('/survey/tpp/:surveyHeaderID', {
+            templateUrl: 'app/survey/tpp/tpp.html',
+            controller: 'TppSurveyCtrl',
+            resolve: {
+                user: ['Auth', function (Auth) {
+                    return Auth.$waitForAuth();
+                }]
+            }
+        });
+    }]);
+
+
+})(angular);
