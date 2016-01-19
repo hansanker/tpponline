c2e7e61e3a6ca6c2246e73efc796d08ffe5fbbaa
hansanker <ankersupport@gmail.com>
2016-01-19 17:24:39 +0100

    minor login issue

--- app/login/login.js
+++ app/login/login.js
@@ -84,10 +84,9 @@ angular.module('myApp.login', ['firebase.utils', 'firebase.auth', 'ngRoute'])
                         });
                     })
                     .then(function (/* user */) {
-                        // redirect to the account page
+                        
                         // $location.path('/account');
-                        window.history.back();
-                        //window.history.back();
+                        $window.history.back();
                     }, function (err) {
                         $scope.err = errMessage(err);
                     });
