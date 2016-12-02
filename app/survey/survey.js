2e8ada5eec4ab459749e36804f01cf2ad0498c62
hans anker <ankersupport@gmail.com>
2016-12-02 16:23:12 +0100

    remove console.log

--- app/survey/survey.js
+++ app/survey/survey.js
@@ -7,14 +7,14 @@
         var surveyHeaderID = $routeParams.surveyHeaderID;
 
         /* TODO 1: Check if user already has this survey in their profile */
-        console.log(user.uid);
+        
    
         
         var ref = fbutil.ref();
         ref.once("value", function (snapshot) {
             //if (snapshot.child("users/2f75583e-4199-4424-83ad-f6486e4b45e9/surveys/-K-t52g5-osgYHQ_xJir").exists()) {
             if (snapshot.child("users/" + user.uid + "/surveys/" + surveyHeaderID).exists()) {
-                console.log("OK")
+                
             } else {
                 /* TODO 2: If user does not yet have survey in their profile, add it */
                 ref.child("/users/" + user.uid + "/surveys/" + surveyHeaderID).set(true);
@@ -23,7 +23,7 @@
         
         /* TODO 3: Fetch /SurveyHeaders/<surveyHeaderID> and check its type (template.nameSurvey) */
         ref.child("SurveyHeaders/" + surveyHeaderID + "/template/nameSurvey").once("value", function (data) {
-            console.log(data.val());
+            
             var surveyType = data.val().toLowerCase();
 
             if(['tpp', 'gallop'].indexOf(surveyType) !== -1) {
